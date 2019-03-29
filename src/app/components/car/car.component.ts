5a4f0cb0730aa9224232267ca0fcd85e3e2648bb
ALichoshva <sashalihoshva@gmail.com>
2019-03-29 13:35:50 +0400

    HW3

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -5,11 +5,33 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './car.component.html',
   styleUrls: ['./car.component.css']
 })
-export class CarComponent implements OnInit {
+export class CarComponent {
+  public carName: string;
+  private mileage: number;
+  public fuelTankCapacity: number;
+  private currentAmountOfGas: number;
+  public specifications: string[];
 
+  public car = {
+    carName: 'BMW',
+    mileage: 0,
+    fuelTankCapacity: 20,
+    currentAmountOfGas: 10,
+    specifications: [
+      'Engine: 2 liters',
+      'Maximum speed: 200 km/h',
+      'Production year: 2005',
+      'from 0 to 100: 10 sec.'
+    ]
+  }
   constructor() { }
 
-  ngOnInit() {
+  public go() {
+    this.car.mileage++
+    this.car.currentAmountOfGas--
   }
 
+  public fillUp() {
+    this.car.currentAmountOfGas++
+  }
 }
